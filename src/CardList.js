3d7487bb9c7201f7a3258d19cf05ac88f5f143cf
npxrus <npxrus@yandex.com>
2022-06-16 19:46:12 +0300

    feat: add CardList component

---
+++ src/CardList.js
@@ -0,0 +1,13 @@
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+  return (
+    <>
+      {robots.map((robot) => {
+        return <Card key={robot.id} id={robot.id} name={robot.name} email={robot.email} />;
+      })}
+    </>
+  );
+};
+
+export default CardList;
