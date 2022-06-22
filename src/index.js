fc2634a43f0936a19b70e8a9fe25f12ea8ec85bf
npxrus <npxrus@yandex.com>
2022-06-22 13:43:58 +0300

    fix: fix index.js to use React 18 features

--- src/index.js
+++ src/index.js
@@ -4,9 +4,4 @@ import './index.css';
 import 'tachyons';
 import App from './containers/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
