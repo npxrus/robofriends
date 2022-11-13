aa3dcfd5a63ada708e8f5dd45566dba036a29fb4
npxrus <npxrus@yandex.com>
2022-11-13 17:41:53 +0300

    feat: add App component and styles

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      robots,
+      searchField: '',
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) =>
+      robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    );
+
+    return (
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'tachyons';
-import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CardList />
+    <App />
   </React.StrictMode>
 );
 
