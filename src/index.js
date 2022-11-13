53662474119e64e345ae58b1b5b6806c616fe6df
npxrus <npxrus@yandex.com>
2022-11-13 16:30:49 +0300

    feat: add Card and CardList components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import 'tachyons';
+import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CardList />
   </React.StrictMode>
 );
 
