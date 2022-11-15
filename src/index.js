00df385a2ea2c2cd9d69a3bcf16d05af7abb51d5
npxrus <npxrus@yandex.com>
2022-11-15 21:01:13 +0300

    feat: add logger middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 import App from './containers/App';
 import { searchRobots } from './reducers';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
