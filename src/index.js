5dd159cffd33dcda5b74c8ccc4086e7faee15fa1
npxrus <npxrus@yandex.com>
2022-11-15 20:08:08 +0300

    feat: add a store to app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 import App from './containers/App';
+import { searchRobots } from './reducers';
+
+const store = createStore(searchRobots);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
